faa718d1affece0e73f09cf43ec789b8f4e056a3
l2xu <weihrauch.business@gmail.com>
2021-02-24 11:05:43 +0100

    lol

--- js/app.js
+++
@@ -1,6 +0,0 @@
-const schriftzug = document.querySelectorAll("#schriftzug path");
-for (let i = 0; i < schriftzug.length; i++) {
-    console.log(`Letter ${i} is ${schriftzug[i].getTotalLength()}`);
-
-}
-
